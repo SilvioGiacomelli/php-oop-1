d24805f2bfa33bfd12eada2f2554ea5d340065a3
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-05-02 15:05:15 +0200

    [Inseriti film]

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 require_once __DIR__ . '/Model/Movie.php';
+
+$alien = new Movie('Alien', 'Ridley Scott', 1979, 'Horror', 8.5, 1);
+$lordOfTheRings = new Movie('The Lord of the Rings: The Fellowship of the Ring', 'Peter Jackson', 2001, 'Adventure', 8.8, 2);
+$xxx = new Movie('xXx', 'Rob Cohen', 2002, 'Action', 5.9, 3);
+$viaColVento = new Movie('Via col vento', 'Victor Fleming', 1939, 'Drama', 8.1, 4);
+$limitless = new Movie('Limitless', 'Neil Burger', 2011, 'Mystery', 7.4, 5);
+$thewolfOfWallStreet = new Movie('The Wolf of Wall Street', 'Martin Scorsese', 2013, 'Biography', 8.2, 6);
+$cloverfield = new Movie('Cloverfield', 'Matt Reeves', 2008, 'Horror', 7.0, 7);
+$schindlersList = new Movie('Schindler\'s List', 'Steven Spielberg', 1993, 'Biography', 8.9, 8);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
